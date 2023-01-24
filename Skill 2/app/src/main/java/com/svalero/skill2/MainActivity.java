1842d3d7d8bda65e17630c858dc41ec51b466ee6
tmaarcxs <marcos.sanchez.ruda@gmail.com>
2023-01-24 13:50:50 +0100

    video 29

--- Skill 2/app/src/main/java/com/svalero/skill2/MainActivity.java
+++ Skill 2/app/src/main/java/com/svalero/skill2/MainActivity.java
@@ -3,6 +3,7 @@ package com.svalero.skill2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "Hola", Toast.LENGTH_LONG).show();
     }
 }
